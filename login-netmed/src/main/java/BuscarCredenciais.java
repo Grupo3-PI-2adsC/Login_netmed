c027508f27d80733d5085f944c063a41fbdc90a6
MatteusNogueira <matteus.bins@sptech.school>
2024-04-11 04:09:07 -0300

    Começo da personalização da API Looca / Junção do JAVA login com bd

--- login-netmed/src/main/java/BuscarCredenciais.java
+++ login-netmed/src/main/java/BuscarCredenciais.java
@@ -1,30 +1,45 @@
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BuscarCredenciais {
 
-//    List <String> email = new ArrayList();
-//    List <String> senha = new ArrayList();
-//
-//    void cadastrarUsuario(String emailUser, String senhaUser){
-//
-//        email.add(emailUser);
-//
-//        senha.add(senhaUser);
-//
-//    }
 
-     public String buscarEmail(){
-        String email = "matteus@gmail.com";
+    String url = "jdbc:mysql://localhost:3306/_coloque_nome_database_";
+    String username = "_coloque_user_mysql_";
+    String password = "_coloque_senha_mysql_";
 
-        return email;
 
-    };
+    public Boolean validarCredenciais(String email, String senha){
 
-    public String buscarSenha(){
-        String senha = "matt@123";
+        Boolean retornoValidar = false;
 
-        return senha;
-    };
+        Connection con = null;
 
+        String sql = "select * from testeJava where email = '%s' and senha = '%s';".formatted(email,senha);
+
+
+        try {
+            con = DriverManager.getConnection(url, username, password);
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(sql);
+
+            rs.next();
+
+            String id = rs.getString(1);
+
+            System.out.println("O id do usuario é " + id);
+
+            con.close();
+
+            retornoValidar = true;
+
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        }
+        return retornoValidar;
+    }
 }
